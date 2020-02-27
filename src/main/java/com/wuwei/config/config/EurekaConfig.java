634136253ec57ca7a7e6b84218ecad277bccb1f7
吴威 <664507439@qq.com>
2020-02-27 21:50:11 +0800

    no message

---
+++ src/main/java/com/wuwei/config/config/EurekaConfig.java
@@ -0,0 +1,11 @@
+package com.wuwei.config.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: yexiaodong
+ * @Date:2018/9/8 11:02
+ */
+@EnableEurekaClient
+public class EurekaConfig {
+}
